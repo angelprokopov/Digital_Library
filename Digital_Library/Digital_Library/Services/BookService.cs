5c3b0781c3a5b63676c69f17fbc63b7127115c1d
angelprokopov <81374123+angelprokopov@users.noreply.github.com>
2025-01-21 23:53:59 +0200

    ASP.NET Razor web app which allows the user to view the books that he owns in a view of a digital library

---
+++ Digital_Library/Digital_Library/Services/BookService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SharedModels;
+
+namespace Digital_Library.Services
+{
+    public class BookService
+    {
+        private readonly HttpClient _httpClient;
+        public BookService(HttpClient httpClient) { _httpClient = httpClient; }
+
+        public async Task<List<Book>> GetBooksAsync()
+        {
+            var response = await _httpClient.GetAsync("https://bookdetailsapi.azurewebsites.net/api/Books");
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadFromJsonAsync<List<Book>>();
+
+            throw new HttpRequestException($"Failed to fetch books: {response.ReasonPhrase}");
+        }
+
+        public async Task<Book> GetBookByISBNAsync(string isbn)
+        {
+            return await _httpClient.GetFromJsonAsync<Book>($"api/Books/{isbn}");
+        }
+
+        public async Task<Book> AddBookAsync(Book book)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/Books", book);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<Book>();
+        }
+
+        public async Task<List<Book>> SearchBookByTitleAsync(string title)
+        {
+            var response = await _httpClient.GetAsync($"api/Books/search?title={Uri.EscapeDataString(title)}");
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<List<Book>>();
+            }
+            else if(response.StatusCode==System.Net.HttpStatusCode.NotFound)
+            {
+                return new List<Book>();
+            }
+
+            throw new HttpRequestException($"Error fetching books: {response.ReasonPhrase}");
+        }
+    }
+}
